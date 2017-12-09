d78af1f5c8f2042114d758e0d6674055c2a16a1b
seonghoonkang <kangpual@naver.com>
2017-12-09 14:53:16 +0900

    Update todo-detail.component.ts

--- BNBProject/src/app/todo/detail/todo-detail.component.ts
+++ BNBProject/src/app/todo/detail/todo-detail.component.ts
@@ -23,6 +23,7 @@ export class TodoDetailComponent implements OnInit {
         // keyword : 절차형 프로그래밍, 반응형 프로그래밍, 데이터 스트림, BehaviorSubject, Observer
         // ActivatedRoute서비스의 Observable 타입의 params를 제공한다. Angular2는 Rxjs를 적극 도입하여 활용하도록 했다.
         // params에 값이 바인딩되면 해당 결과를 subscribe하여 결과를 수행한다.
+        // this._router.snapshot.paramMap.get("id"); //
         this._router.params
             .switchMap((params: Params) => this.todoService.getTodo(+params['id']))// (+) converts string 'id' to a number
             .subscribe(todo => this.todoDetail = todo);
